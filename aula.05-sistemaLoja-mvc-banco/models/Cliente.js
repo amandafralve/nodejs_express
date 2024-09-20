dba4c1897a9e8c69bfd1219dab9c5f93cf5e7c39
amandafralve <amanda.fralve@gmail.com>
2024-09-20 20:08:23 -0300

    aula05

---
+++ aula.05-sistemaLoja-mvc-banco/models/Cliente.js
@@ -0,0 +1,23 @@
+// ORM - SEQUELIZE
+import Sequelize from 'sequelize';
+// Config sequelize
+import connection from '../config/sequelize-config.js'
+
+// .define cria a tabela no banco
+const Cliente = connection.define('clientes',{
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cpf: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    endereco: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    
+});
+Cliente.sync({force: false});
+export default connection;
