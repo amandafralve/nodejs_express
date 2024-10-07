da99ab0b22879ec90036296634f03b9199bbedd1
Amanda <amanda.fralve@gmail.com>
2024-10-07 17:05:16 -0300

    att view/controller produtos

--- aula.05-sistemaLoja-mvc-banco/config/sequelize-config.js
+++ aula.05-sistemaLoja-mvc-banco/config/sequelize-config.js
@@ -6,7 +6,7 @@ const connection = new Sequelize({
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: '',
+    password: 'Amanda15##',
     // Comentar esta linha na primeira execução da aplicação
     database: 'loja',
     timezone: '-03:00'
--- aula.05-sistemaLoja-mvc-banco/controllers/ProdutosController.js
+++ aula.05-sistemaLoja-mvc-banco/controllers/ProdutosController.js
@@ -26,4 +26,51 @@ router.post("/produtos/new", (req,res) => {
     })
 })
 
+// ROTA DE EXCLUSÃO DE PRODUTO
+router.get("/produtos/delete/:id", (req,res) => {
+    const id = req.params.id;
+    // MÉTODO EXCLUIR
+    Produto.destroy({
+        where: {
+            id:id
+        }
+    }).then(()=>{
+        res.redirect("/produtos/")
+    }).catch((error) => {
+        console.log(error);
+    })
+})
+
+// ROTA EDICAO DADOS PRODUTO
+router.get("/produtos/edit/:id", (req,res) =>{
+    const id = req.params.id;
+    Produto.findByPk(id).then((produto) => {
+        res.render("produtoEdit",{
+            produto:produto
+        })
+    }).catch((error) => {
+        console.log(error);
+    })
+})
+
+//ROTA ALTERACAO DADOS PRODUTO
+router.post("/produtos/update", (req,res) =>{
+    const {id, nome, preco, categoria} = req.body;
+
+    Produto.update(
+        {
+            nome:nome,
+            preco:preco,
+            categoria:categoria
+        },
+        {
+            where: {id:id}
+        }
+    ).then(()=>{
+        res.redirect("/produtos")
+    }).catch((error) =>{
+        console.log(error)
+    })
+})
+
 export default router
